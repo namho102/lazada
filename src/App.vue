29ac22c59a802a96fc2f7982b3ee2e2392b4b5fd
Hoang Nam <namhoang.td09@gmail.com>
2017-04-02 21:01:37 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,219 @@
+<template>
+  <div id="app">
+    <header>
+
+        <!-- *** TOP ***
+_________________________________________________________ -->
+        <div id="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-5 contact">
+                    </div>
+                    <div class="col-xs-7">
+                        <div class="social">
+                            <a href="#" class="external facebook" data-animate-hover="pulse"><i class="fa fa-facebook"></i></a>
+                            <a href="#" class="external gplus" data-animate-hover="pulse"><i class="fa fa-google-plus"></i></a>
+                            <a href="#" class="external twitter" data-animate-hover="pulse"><i class="fa fa-twitter"></i></a>
+                            <a href="#" class="email" data-animate-hover="pulse"><i class="fa fa-envelope"></i></a>
+                        </div>
+
+                        <div class="login">
+                            <a href="#" data-toggle="modal" data-target="#login-modal"><i class="fa fa-sign-in"></i> <span class="hidden-xs text-uppercase">Sign in</span></a>
+                            <a href="customer-register.html"><i class="fa fa-user"></i> <span class="hidden-xs text-uppercase">Sign up</span></a>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- *** TOP END *** -->
+
+        <!-- *** NAVBAR ***
+_________________________________________________________ -->
+
+      <div class="row row2">
+         <div class="col-sm-2">
+
+             <h1 class="brand">Lazada</h1>
+         </div>
+         <div class="flipkart-navbar-search smallsearch col-sm-8 col-xs-11">
+             <div class="row">
+                 <input class="flipkart-navbar-input col-xs-11" type="" placeholder="Search for Products" name="">
+                 <button class="flipkart-navbar-button col-xs-1">
+                     <svg width="15px" height="15px">
+                         <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
+                     </svg>
+                 </button>
+             </div>
+         </div>
+         <div class="cart largenav col-sm-2">
+             <a class="cart-button">
+                 <svg class="cart-svg " width="16 " height="16 " viewBox="0 0 16 16 ">
+                     <path d="M15.32 2.405H4.887C3 2.405 2.46.805 2.46.805L2.257.21C2.208.085 2.083 0 1.946 0H.336C.1 0-.064.24.024.46l.644 1.945L3.11 9.767c.047.137.175.23.32.23h8.418l-.493 1.958H3.768l.002.003c-.017 0-.033-.003-.05-.003-1.06 0-1.92.86-1.92 1.92s.86 1.92 1.92 1.92c.99 0 1.805-.75 1.91-1.712l5.55.076c.12.922.91 1.636 1.867 1.636 1.04 0 1.885-.844 1.885-1.885 0-.866-.584-1.593-1.38-1.814l2.423-8.832c.12-.433-.206-.86-.655-.86 " fill="#fff "></path>
+                 </svg> Link
+                 <span class="item-number ">0</span>
+             </a>
+         </div>
+      </div>
+
+        <!-- *** NAVBAR END *** -->
+
+    </header>
+
+    <!-- *** LOGIN MODAL ***  -->
+
+    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="Login">Customer login</h4>
+                </div>
+                <div class="modal-body">
+                    <form action="customer-orders.html" method="post">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="email_modal" placeholder="email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password_modal" placeholder="password">
+                        </div>
+
+                        <p class="text-center">
+                            <button class="btn btn-template-main"><i class="fa fa-sign-in"></i> Log in</button>
+                        </p>
+
+                    </form>
+
+                    <p class="text-center text-muted">Not registered yet?</p>
+                    <p class="text-center text-muted"><a href="customer-register.html"><strong>Register now</strong></a>! It is easy and done in 1&nbsp;minute and gives you access to special discounts and much more!</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- *** LOGIN MODAL END *** -->
+
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped>
+  header {
+      background-color: #555555;
+      margin-botton: 20px;
+  }
+
+
+  .row2 {
+      padding-bottom: 20px;
+
+  }
+
+  .flipkart-navbar-input {
+      padding: 8px 16px;
+      border-radius: 2px 0 0 2px;
+      border: 0 none;
+      outline: 0 none;
+      font-size: 15px;
+  }
+
+  .flipkart-navbar-button {
+      background-color: #ffe11b;
+      border: 1px solid #ffe11b;
+      border-radius: 0 2px 2px 0;
+      color: #565656;
+      padding: 10px 0;
+      height: 37px;
+      cursor: pointer;
+  }
+
+  .cart-button {
+      background-color: #2469d9;
+      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .23), inset 1px 1px 0 0 hsla(0, 0%, 100%, .2);
+      padding: 7px 0;
+      text-align: center;
+      height: 37px;
+      border-radius: 2px;
+      font-weight: 500;
+      width: 120px;
+      display: inline-block;
+      color: #FFFFFF;
+      text-decoration: none;
+      color: inherit;
+      border: none;
+      outline: none;
+  }
+
+  .cart-button:hover{
+      text-decoration: none;
+      color: #fff;
+      cursor: pointer;
+  }
+
+  .cart-svg {
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      vertical-align: middle;
+      margin-right: 8px;
+  }
+
+  .item-number {
+      border-radius: 3px;
+      background-color: rgba(0, 0, 0, .1);
+      height: 20px;
+      padding: 3px 6px;
+      font-weight: 500;
+      display: inline-block;
+      color: #fff;
+      line-height: 12px;
+      margin-left: 10px;
+  }
+
+  h1.brand {
+    color: #fff;
+    margin: 0px;
+    margin-left: 20px;
+  }
+  .largenav {
+      display: none;
+  }
+
+  .smallnav{
+      display: block;
+  }
+
+  .smallsearch{
+      margin-left: 15px;
+      margin-top: 15px;
+  }
+
+  .menu{
+      cursor: pointer;
+  }
+
+  @media screen and (min-width: 768px) {
+      .largenav {
+          display: block;
+      }
+      .smallnav{
+          display: none;
+      }
+      .smallsearch{
+          margin: 0px;
+      }
+  }
+
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,316 @@
+<template>
+    <div id="content">
+        <div class="container">
+
+            <div class="row">
+
+
+                <!-- *** LEFT COLUMN ***
+  _________________________________________________________ -->
+
+                <div class="col-sm-3">
+
+                    <!-- *** MENUS AND FILTERS ***
+_________________________________________________________ -->
+                    <div class="panel panel-default sidebar-menu">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Categories</h3>
+                        </div>
+
+                        <div class="panel-body">
+                            <ul class="nav nav-pills nav-stacked category-menu">
+                                <li>
+                                    <a href="shop-category.html">Men</a>
+                                </li>
+                                <li class="active">
+                                    <a href="shop-category.html">Ladies  </a>
+                                </li>
+                                <li>
+                                    <a href="shop-category.html">Kids </a>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    </div>
+
+                    <div class="panel panel-default sidebar-menu">
+
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Brands</h3>
+                            <a class="btn btn-xs btn-danger pull-right" href="#"><i class="fa fa-times-circle"></i> <span class="hidden-sm">Clear</span></a>
+                        </div>
+
+                        <div class="panel-body">
+
+                            <form>
+                                <div class="form-group">
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox">Armani (10)
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox">Versace (12)
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox">Carlo Bruni (15)
+                                        </label>
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input type="checkbox">Jack Honey (14)
+                                        </label>
+                                    </div>
+                                </div>
+
+                                <button class="btn btn-default btn-sm btn-template-main"><i class="fa fa-pencil"></i> Apply</button>
+
+                            </form>
+
+                        </div>
+                    </div>
+
+
+
+                    <!-- *** MENUS AND FILTERS END *** -->
+
+
+                    <!-- /.banner -->
+
+                </div>
+                <!-- /.col-md-3 -->
+
+                <!-- *** LEFT COLUMN END *** -->
+
+                <!-- *** RIGHT COLUMN ***
+  _________________________________________________________ -->
+
+                <div class="col-sm-9">
+
+
+
+                    <div class="row products">
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product1.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">Fur coat with very but very very long name</a></h3>
+                                    <p class="price">$143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+                                </div>
+                                <!-- /.text -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product2.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">White Blouse Armani</a></h3>
+                                    <p class="price"><del>$280</del> $143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+                                </div>
+                                <!-- /.text -->
+
+                                <div class="ribbon sale">
+                                    <div class="theribbon">SALE</div>
+                                    <div class="ribbon-background"></div>
+                                </div>
+                                <!-- /.ribbon -->
+
+                                <div class="ribbon new">
+                                    <div class="theribbon">NEW</div>
+                                    <div class="ribbon-background"></div>
+                                </div>
+                                <!-- /.ribbon -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product3.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                                    <p class="price">$143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+
+                                </div>
+                                <!-- /.text -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product3.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">Black Blouse Versace</a></h3>
+                                    <p class="price">$143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+
+                                </div>
+                                <!-- /.text -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product2.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">White Blouse Versace</a></h3>
+                                    <p class="price">$143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+
+                                </div>
+                                <!-- /.text -->
+
+                                <div class="ribbon new">
+                                    <div class="theribbon">NEW</div>
+                                    <div class="ribbon-background"></div>
+                                </div>
+                                <!-- /.ribbon -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+
+                        <div class="col-md-4 col-sm-6">
+                            <div class="product">
+                                <div class="image">
+                                    <a href="shop-detail.html">
+                                        <img src="static/img/product1.jpg" alt="" class="img-responsive image1">
+                                    </a>
+                                </div>
+                                <!-- /.image -->
+                                <div class="text">
+                                    <h3><a href="shop-detail.html">Fur coat</a></h3>
+                                    <p class="price">$143.00</p>
+                                    <p class="buttons">
+                                        <a href="shop-detail.html" class="btn btn-default">View detail</a>
+                                        <a href="shop-basket.html" class="btn btn-template-main"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                                    </p>
+
+                                </div>
+                                <!-- /.text -->
+                            </div>
+                            <!-- /.product -->
+                        </div>
+                        <!-- /.col-md-4 -->
+                    </div>
+                    <!-- /.products -->
+<!--
+                    <div class="row">
+
+                        <div class="col-md-12 banner">
+                            <a href="#">
+                                <img src="img/banner2.jpg" alt="" class="img-responsive">
+                            </a>
+                        </div>
+
+                    </div> -->
+
+
+                    <div class="pages">
+
+                        <p class="loadMore">
+                            <a href="#" class="btn btn-template-main"><i class="fa fa-chevron-down"></i> Load more</a>
+                        </p>
+
+                        <!-- <ul class="pagination">
+                            <li><a href="#">&laquo;</a>
+                            </li>
+                            <li class="active"><a href="#">1</a>
+                            </li>
+                            <li><a href="#">2</a>
+                            </li>
+                            <li><a href="#">3</a>
+                            </li>
+                            <li><a href="#">4</a>
+                            </li>
+                            <li><a href="#">5</a>
+                            </li>
+                            <li><a href="#">&raquo;</a>
+                            </li>
+                        </ul> -->
+                    </div>
+
+
+                </div>
+                <!-- /.col-md-9 -->
+
+                <!-- *** RIGHT COLUMN END *** -->
+
+            </div>
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /#content -->
+
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #content {
+    margin-top: 30px;
+  }
+</style>
